962b87b2e238ea64f6a1f1c8e1d070ba9568f27b
Fabio Cruz <fabinhocosta1202@gmail.com>
2020-06-18 19:26:47 -0300

    Create table clients

---
+++ src/app/Client.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Client extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 
+        'email', 
+        'personalNumber', 
+        'phone', 
+        'company_id',
+        'zipcode', 
+        'address', 
+        'houseNumber',
+        'neighborhood', 
+        'complement'
+    ];
+}
